9a35fb4024892e146e7f7acddd5f1b5056ef0d0e
Enrick Gornez <john.gornez.20@usjr.edu.ph>
2022-07-13 00:03:27 +0800

    Integrated authentication

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,5 +1,20 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../models/auth.dart';
+
 class ApiService {
   static const String baseUrl = 'https://fakestoreapi.com';
 
-
+  Future<dynamic> login(String username, String password) {
+    final credentials = Auth(username: username, password: password);
+    return http
+        .post(Uri.parse('$baseUrl/auth/login'), body: credentials.toJson())
+        .then((data) {
+      if (data.statusCode == 200) {
+        final jsonData = json.decode(data.body);
+        return jsonData;
+      }
+    }).catchError((err) => print(err));
+  }
 }
